c741b48b03a73d8e8674197740467a80c6db35b6
eric-gebus <ericgebus@gmail.com>
2025-04-25 14:33:05 +0100

    Setup Vitest for Client and added tests for Footer

---
+++ client/src/component/Footer.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen} from "@testing-library/react";
+import Footer from "./Footer.tsx";
+import { describe, it, expect } from "vitest";
+import "@testing-library/jest-dom/vitest";
+
+describe ("Footer", () => {
+  it("renders copyright", () => {
+    render(<Footer />);
+    expect(screen.getByText(/© 2025 All Rights Reserved./i)).toBeInTheDocument();
+  })
+
+  it("renders FlyTyper", () => {
+    expect(screen.getByTestId("flytyper-footer")).toBeInTheDocument();
+  })
+})
